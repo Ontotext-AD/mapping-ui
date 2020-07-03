5f993a395cfad9e7efe677b9acf85d23d963877f
desislava <desislava.hristova@ontotext.com>
2020-07-03 14:04:05 +0300

    Fix mapping save

--- src/app/services/channel-name.enum.ts
+++ src/app/services/channel-name.enum.ts
@@ -1,5 +1,6 @@
 export enum ChannelName {
   SaveMapping = 'save_mapping',
+  MappingSaved = 'mapping_saved',
   NewMapping = 'new_mapping',
   PreviewMapping = 'preview_mapping',
   GetRDF = 'get_RDF',
