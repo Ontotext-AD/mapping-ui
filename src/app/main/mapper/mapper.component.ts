2731078964017b9e4d3b904ee7858b42dd88e911
teodossi.dossev@ontotext.com </d/dP.djd55>
2020-07-27 14:10:23 +0300

    GDB-4830 Sync empty mapping set on New mapping

--- src/app/main/mapper/mapper.component.ts
+++ src/app/main/mapper/mapper.component.ts
@@ -61,8 +61,7 @@ export class MapperComponent extends OnDestroyMixin implements OnInit {
     this.messageService.read(ChannelName.NewMapping)
         .pipe(untilComponentDestroyed(this))
         .subscribe(() => {
-          this.mapping = new MappingDefinitionImpl(EMPTY_MAPPING.baseIRI, EMPTY_MAPPING.namespaces, EMPTY_MAPPING.subjectMappings);
-          this.rdfMapping.next(this.mapping);
+          this.rdfMapping.next(new MappingDefinitionImpl(EMPTY_MAPPING.baseIRI, EMPTY_MAPPING.namespaces, EMPTY_MAPPING.subjectMappings));
         });
 
     this.messageService.read(ChannelName.SaveMapping)
