e0e02fae10a62a708cd440f56948cf73190b6152
Svilen Velikov <svilen.velikov@sirma.bg>
2020-08-05 08:29:17 +0300

    Install the cypress-failed-logs plugin in order to have more detailed logs for the failed tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,3 +4,9 @@ module.exports = (on) =>
 {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
 };
+
+module.exports = (on, config) => {
+  on('task', {
+    failed: require('cypress-failed-log/src/failed')(),
+  })
+};
