d17f8acf40db53dc5c3c911c58c1c494e62be803
Pavel Mihaylov <pavel@ontotext.com>
2020-11-16 14:48:18 +0200

    GDB-5231: Get the Authorization header value from local storage (new way) instead of cookie (old way), also updated the version to 1.0.2-TR2.

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -7,17 +7,19 @@ import {
 } from '@angular/common/http';
 import {CookieService} from 'ngx-cookie-service';
 import {Observable} from 'rxjs';
-import {RestService} from '../services/rest/rest.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   constructor(protected cookies: CookieService) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-      setHeaders: {
-        Authorization: this.cookies.get('com.ontotext.graphdb.auth' + RestService.getPort()),
-      },
-    });
+    const auth = localStorage.getItem('com.ontotext.graphdb.auth');
+    if (auth) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: auth,
+        },
+      });
+    }
     return next.handle(request);
   }
 }
